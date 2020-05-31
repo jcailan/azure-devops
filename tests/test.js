c0527e7e3378287cee3619c5ea807d487f42ec5e
Jhodel Cailan <jhodel.cailan@gmail.com>
2020-05-31 16:48:18 +0800

    Added mocha testing

---
+++ tests/test.js
@@ -0,0 +1,48 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("./server");
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+let app = null;
+
+before((done) => {
+	server.then((result) => {
+		app = result;
+		done();
+	});
+});
+
+describe("Products Operation", () => {
+	describe("GET /catalog/Products", () => {
+		it("+ should return a list of products", (done) => {
+			chai.request(app)
+				.get("/catalog/Products")
+				.end((error, response) => {
+					try {
+						response.should.have.status(200);
+						response.body.value.should.be.an("array").to.have.lengthOf(12);
+						done();
+					} catch (error) {
+						done(error);
+					}
+				});
+		});
+
+		it("+ should return a list of products", (done) => {
+			chai.request(app)
+				.get("/catalog/Products")
+				.end((error, response) => {
+					try {
+						response.should.have.status(200);
+						response.body.value.should.be.an("array").to.have.lengthOf(2);
+						done();
+					} catch (error) {
+						done(error);
+					}
+				});
+		});
+	});
+});
